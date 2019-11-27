79f1351060a72ffdb07ae27337d915e71697821e
Ramón Rodríguez <rrperochena@gmail.com>
2019-11-27 20:19:23 +0100

    get_next_line

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ramrodri <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/27 19:25:22 by ramrodri          #+#    #+#             */
+/*   Updated: 2019/11/27 20:03:53 by ramrodri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 8
+# define FD_SIZE 4096
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+
+int	get_next_line(const int fd, char **line);
+char	*ft_strdup(const char *s1);
+void	ft_strdel(char **as);
+void	ft_memdel(void **ap);
+char	*ft_strsub(char const *s, unsigned int start, size_t len);
+char	*ft_strnew(size_t size);
+void	*ft_calloc(size_t count, size_t size);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strcpy(char *dest, const char *src);
+char	*ft_strcat(char *dest, const char *src);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+void    ft_bzero(void *s, size_t n);
+void    *ft_memset(void *s, int c, size_t n);
+#endif
---
+++ get_next_line/get_next_line_utils.c
@@ -0,0 +1,150 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ramrodri <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/27 19:38:57 by ramrodri          #+#    #+#             */
+/*   Updated: 2019/11/27 19:43:11 by ramrodri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strdup(const char *s1)
+{
+	char	*new;
+	int		i;
+	int		size;
+
+	size = 0;
+	while (s1[size])
+		++size;
+	if (!(new = malloc(sizeof(char) * (size + 1))))
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		new[i] = s1[i];
+		i++;
+	}
+	new[i] = '\0';
+	return (new);
+}
+
+void	ft_strdel(char **as)
+{
+	if (as != NULL && *as != NULL)
+		ft_memdel((void**)as);
+}
+
+void	ft_memdel(void **ap)
+{
+	if (ap != NULL)
+	{
+		free(*ap);
+		*ap = NULL;
+	}
+}
+
+char	*ft_strsub(char const *s, unsigned int start, size_t len)
+{
+	char	*subs;
+	size_t	i;
+
+	if (s == NULL)
+		return (NULL);
+	subs = ft_strnew(len);
+	if (subs == NULL)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		subs[i] = s[start + i];
+		i++;
+	}
+	return (subs);
+}
+
+char	*ft_strnew(size_t size)
+{
+	return (char *)ft_calloc(1, sizeof(char) * (size + 1));
+}
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	char	*str;
+
+	str = NULL;
+	str = malloc(count * size);
+	if (str != NULL)
+		ft_bzero(str, count * size);
+	return (str);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char *res;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	if ((res = ft_strnew(ft_strlen(s1) + ft_strlen(s2))) == NULL)
+		return (NULL);
+	ft_strcpy(res, s1);
+	ft_strcat(res, s2);
+	return (res);
+}
+
+char	*ft_strcpy(char *dest, const char *src)
+{
+	size_t i;
+
+	i = -1;
+	while (src[++i])
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*ft_strcat(char *dest, const char *src)
+{
+	size_t	i;
+	char	*s;
+
+	i = ft_strlen(dest);
+	s = (char *)src;
+	while (*s)
+		dest[i++] = *s++;
+	dest[i] = '\0';
+	return (dest);
+}
+
+size_t		ft_strlen(const char *s)
+{
+	size_t i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int		i;
+	char	*ss;
+
+	i = 0;
+	ss = (char *)s;
+	while (ss[i])
+	{
+		if ((char)c == ss[i])
+			return (ss + i);
+		i++;
+	}
+	if ((char)c == ss[i])
+		return (ss + i);
+	return (NULL);
+}
+
